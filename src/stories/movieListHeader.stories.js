a28c359b40db25c3d126ea05f4260cdf883605e4
htkelly <hrykly@gmail.com>
2022-06-26 16:00:11 +0100

    Fix broken stories in Storybook.

--- src/stories/movieListHeader.stories.js
+++ src/stories/movieListHeader.stories.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { MemoryRouter } from "react-router";
 import MovieListHeader from "../components/headerMovieList";
 
 export default {
   title: "Home Page/Header",
   component: MovieListHeader,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+  ],
 };
 
 export const Basic = () => <MovieListHeader title={'Discover Movies'} />;
